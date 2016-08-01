42369c7fc42b43bb081f1417311f3c74d6610fd0
吴存勇 <cywu88@126.com>
2016-08-01 10:13:55 +0800

    基础数据

--- SoftRenderer/Form1.cs
+++ SoftRenderer/Form1.cs
@@ -9,9 +9,46 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SoftRenderer {
-    public partial class SoftRendererApp : Form {
+    public partial class SoftRendererApp : Form
+    {
+        private Bitmap _texture;  //纹理
+        private Bitmap _frameBuff;//用一张bitmap来做帧缓冲
+        private Graphics _frameG;
+
+
         public SoftRendererApp() {
             InitializeComponent();
+            try
+            {
+                System.Drawing.Image img = System.Drawing.Image.FromFile("../../Texture/cc.jpg");
+                _texture = new Bitmap(img, 256, 256);
+            }
+            catch (Exception)
+            {
+                _texture = new Bitmap(256, 256);
+                initTexture();
+            }
+
+            _frameBuff = new Bitmap(this.MaximumSize.Width, this.MaximumSize.Height);
+            _frameG = Graphics.FromImage(_frameBuff);
+
+
+
+
+
+    }
+
+
+        public void initTexture()
+        {
+            for (int i = 0; i < 256; i++)
+            {
+                for (int j = 0; j < 256; j++)
+                {
+                    _texture.SetPixel(j, i, ((j + i) % 32 == 0) ? System.Drawing.Color.White : System.Drawing.Color.Green);
+                }
+            }
         }
     }
+ 
 }
